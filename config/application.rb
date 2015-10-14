4b0e85e0f6bb99571f809274cd786f10dea98f10
camilo9020 <camilo9020@gmail.com>
2015-10-14 16:41:53 -0500

    this closes #1, configure devise with sign up, log in and log out features

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ECommerce
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #Devise
+    config.assets.initialize_on_precompile = false
+
   end
 end
