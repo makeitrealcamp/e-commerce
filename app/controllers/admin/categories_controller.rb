61cbbb17b6a02e7af4ac30754ea23569739b8f77
juanfrc <juanfrc2@gmail.com>
2015-10-26 21:58:16 -0500

    Add new category view and controller Create and New action

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,20 @@
 class Admin::CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
 end
