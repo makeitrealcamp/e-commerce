797b2998fffd8d9ca2732efb4d32ef77588ccb3e
camilo9020 <camilo9020@gmail.com>
2015-10-28 17:26:28 -0500

    fixing conflict in scheme

--- db/schema.rb
+++ db/schema.rb
@@ -82,10 +82,14 @@ ActiveRecord::Schema.define(version: 20151026165222) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                                    null: false
     t.datetime "updated_at",                                    null: false
+    t.string   "provider"
+    t.string   "uid"
     t.integer  "role",                   limit: 1, default: 0,  null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["uid"], name: "index_users_on_uid"
 
 end
