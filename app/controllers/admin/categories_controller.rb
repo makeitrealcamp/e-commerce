ff3b1142cd31c6e08b618a32d956026bc5d2810e
juanfrc <juanfrc2@gmail.com>
2015-11-02 15:25:08 -0500

    Add flash danger and success messages to category create and update

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -12,7 +12,9 @@ class Admin::CategoriesController < ApplicationController
     @category.name = @category.name.titleize
     if @category.save
       redirect_to admin_categories_path
+      flash[:success] = "La Categoría se creo exitosamente"
     else
+      flash[:danger] = "Error al crear la Categoría"
       render 'new'
     end
   end
@@ -25,7 +27,9 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     if @category.update(category_params)
       redirect_to admin_categories_path
+      flash[:success] = "La Categoría se edito exitosamente"
     else
+      flash[:danger] = "Error al editar la Categoría"
       render 'edit'
     end
   end
